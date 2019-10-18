c236ca3e6430bf55d37a782f4e632f6c8e6ded5f
mlunceford <mark7lunceford@gmail.com>
2019-10-18 17:00:51 -0600

    5pm

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './reset.css'
 import './App.css';
 import Wizard from './Components/Wizard/Wizard';
 import Dashboard from './Components/Dashboard/Dashboard';
--- src/Components/Wizard/Wizard.js
+++ src/Components/Wizard/Wizard.js
@@ -19,6 +19,7 @@ class Wizard extends React.Component {
     handleInputZip = (num) => {this.setState({zip: num})}
     render() {
         console.log(this.state.zip)
+        const {name, address, city, state, zip} = this.state;
         return (
             <div className="Wizard">
                 <div className="sidebars"></div>
@@ -29,19 +30,29 @@ class Wizard extends React.Component {
                     </div>
                     <h4>Property Name</h4>
                     <input type='text'
+                        
+                        value={name}
                         onChange={e => this.handleInputName(e.target.value)}/>
                     <h4>Address</h4>
                     <input type="text"
+                        maxLength='100'
+                        value={address}
                         onChange={e => this.handleInputAddress(e.target.value)}/>
                     <h4>City</h4>
-                    <input type="text" 
-                    onChange={e => this.handleInputCity(e.target.value)}/>
+                    <input type="text"
+                        maxLength='50'
+                        value={city} 
+                        onChange={e => this.handleInputCity(e.target.value)}/>
                     <h4>State</h4>
-                    <input type="text" 
-                    onChange={e => this.handleInputState(e.target.value)}/>
+                    <input type="text"
+                        maxLength='2'
+                        value={state} 
+                        onChange={e => this.handleInputState(e.target.value)}/>
                     <h4>Zip</h4>
                     <input type="number" 
-                    onChange={e => this.handleInputZip(e.target.value)}/>
+                        maxLength='8'
+                        value={zip}
+                        onChange={e => this.handleInputZip(e.target.value)}/>
                     <button className='completeBtn'>Complete</button>
                 </div>
                 <div className="sidebars"></div>
